aaea8809e4f01840b1846369080d24cc38cb98cb
Sabree31 <36256177+Sabree31@users.noreply.github.com>
2019-07-08 22:48:00 +0600

    Create activitymain.java

---
+++ Sharing Text/activitymain.java
@@ -0,0 +1,38 @@
+package com.example.sharingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editText;
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit);
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, messageText);
+                String chooserTitle = getString(R.string.chooser);
+                Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+                startActivity(chosenIntent);
+            }
+        });
+
+    }
+}
